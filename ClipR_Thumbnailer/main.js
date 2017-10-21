799f4e6b46a4b5c0c16ce2373742cc5e70e28d79
Raviteja Lingineni <rlingineni@live.com>
2017-10-21 15:51:37 -0500

    Changed ClipR_Thumbnailer: Add S3 Recursive Upload init

--- ClipR_Thumbnailer/main.js
+++ ClipR_Thumbnailer/main.js
@@ -5,80 +5,81 @@ const exec = require("child_process").exec;
 var s3 = new AWS.S3();
 
 exports.myHandler = function(event, context, callback) {
-
-
   var contentName = event.content;
-  
+
   exec("ls; cp tmobile.mp4 /tmp/", (error, stdout, stderr) => {
     console.log(`${stdout}`);
     console.log(`${stderr}`);
-
 
     if (error !== null) {
       console.log(`exec error: ${error}`);
       callback("ERROR!");
     }
 
-    var params = {Bucket: 'clipr-videos', Key: contentName + '.mp4'};
-    var file = require('fs').createWriteStream('/tmp/' + contentName+'.mp4');
-    s3.getObject(params).createReadStream().pipe(file);
+    var params = { Bucket: "clipr-videos", Key: contentName + ".mp4" };
+    var file = require("fs").createWriteStream("/tmp/" + contentName + ".mp4");
+    s3
+      .getObject(params)
+      .createReadStream()
+      .pipe(file);
 
+    var outputImageName = contentName + "%d.png";
 
-    var outputImageName = contentName+"%d.png"
-
-    exec("tar xf ffmpeg.tar.xz -C /tmp/; cd /tmp/; ls; mkdir out; ./ffmpeg-3.4-64bit-static/ffmpeg -i " + contentName +'.mp4' + "-vf fps=1 /out/" +outputImageName + " ; ls; ", (error, stdout, stderr) => {
+    exec(
+      "tar xf ffmpeg.tar.xz -C /tmp/; cd /tmp/; ls; mkdir out; ./ffmpeg-3.4-64bit-static/ffmpeg -i " +
+        contentName +
+        ".mp4" +
+        "-vf fps=1 /out/" +
+        outputImageName +
+        " ; ls; ",
+      (error, stdout, stderr) => {
         console.log(`${stdout}`);
         console.log(`${stderr}`);
-        
 
-        const s3Path = path.join(__dirname, "./tmp/out");
-        var bucketName = 'clipr-thumbnails';
-        var key = "out.png";
+        zipFolder("/tmp/out", "/tmp/out.zip", function(err) {
+          if (err) {
+            console.log("oh no!", err);
+            callback("Oh, no!");
+          } else {
+            fs.readFile("/tmp/out.zip", function(err, data) {
+              if (err) {
+                var resp = {
+                  statusCode: 400,
+                  body: JSON.stringify({
+                    msg: "Error Reading Zipped Module",
+                    err: err.message
+                  })
+                };
+                callback(null, resp);
+                throw err;
+              }
 
-        var uploadS3 = function(keyPath) {
-            let fullPath = path.join(s3Path,keyPath);
-            fs.readdirSync(fullPath).forEach( file => {
-                let stats = fs.lstatSync(path.join(fullPath,file));
-                if(stats.isDirectory()) {
-                    key = path.join(key, file);
-                    uploadS3(key);
-                } else if(stats.isFile()) {
-                    params = {Bucket: bucketName, Key: path.join(key, file), Body: fs.readFileSync(path.join(fullPath,file)) };
-                    s3.putObject(params, function(err, data) {
-                        if (err) {
-                            console.log(err)
-                        } else {
-                            console.log('Successfully uploaded data to bucket');
-                        }
-                    });
+              var s3params = {
+                Bucket: "clipr-thumbnails",
+                Key: contentName + ".zip",
+                Body: data
+              };
+              s3.upload(s3params, function(err, data) {
+                if (err) {
+                  var resp = {
+                    statusCode: 400,
+                    body: JSON.stringify({
+                      msg: "Error Saving Code",
+                      err: err.message
+                    })
+                  };
+                  callback(null, resp);
                 }
+              });
             });
-    };
-
-
-        uploadS3(key);
-            
+          }
         });
 
-
-       
         if (error !== null) {
           console.log(`exec error: ${error}`);
           callback("ERROR!");
         }
-
-
-
-
-
-
-      })
-    
-
-
-
-
-
-
+      }
+    );
   });
 };
