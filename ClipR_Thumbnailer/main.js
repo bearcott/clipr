23b2793196c1b83cac4f7acbc694f8e0d9dbaa93
Raviteja Lingineni <rlingineni@live.com>
2017-10-21 14:30:32 -0500

    Changed ClipR_Thumbnailer: Add as tar

--- ClipR_Thumbnailer/main.js
+++ ClipR_Thumbnailer/main.js
@@ -4,7 +4,7 @@ var c = require("child_process");
 const exec = require("child_process").exec;
 
 exports.myHandler = function(event, context, callback) {
-  exec("ls", (error, stdout, stderr) => {
+  exec("ls; mv ffmpeg.tar.xz /tmp/", (error, stdout, stderr) => {
     console.log(`${stdout}`);
     console.log(`${stderr}`);
 
@@ -15,7 +15,7 @@ exports.myHandler = function(event, context, callback) {
     }
 
 
-    exec("tar xf ffmpeg.tar.xz; cd ffmpeg-3.4-64bit-static; ls; ./ffmpeg -version", (error, stdout, stderr) => {
+    exec("cd /tmp/ tar xf ffmpeg.tar.xz; cd ffmpeg-3.4-64bit-static; ls; ./ffmpeg -version", (error, stdout, stderr) => {
         console.log(`${stdout}`);
         console.log(`${stderr}`);
         callback(null, "OK, responsive");
