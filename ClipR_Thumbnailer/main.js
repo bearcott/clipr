3c97f42aefa034d6c42096ec9c41711deaca1094
Raviteja Lingineni <rlingineni@live.com>
2017-10-21 14:02:28 -0500

    Changed ClipR_Thumbnailer: added Thumbnailer Library

--- ClipR_Thumbnailer/main.js
+++ ClipR_Thumbnailer/main.js
@@ -1,1 +1,40 @@
-exports.myHandler = function(event, context, callback) { callback(null,"Hello World") }
+const Thumbler = require("thumbler");
+var AWS = require("aws-sdk");
+var fs = require("fs");
+var c = require("child_process");
+const exec = require("child_process").exec;
+
+exports.myHandler = function(event, context, callback) {
+  exec("ls", (error, stdout, stderr) => {
+    console.log(`${stdout}`);
+    console.log(`${stderr}`);
+    callback(null, "OK, responsive");
+    if (error !== null) {
+      console.log(`exec error: ${error}`);
+      callback("ERROR!");
+    }
+
+    Thumbler(
+      {
+        type: "video",
+        input: "lmao.mp4",
+        output: "/tmp/output.jpeg",
+        time: "00:00:10",
+        size: "300x200" // this optional if null will use the desimention of the video
+      },
+      function(err, path) {
+        exec("cd /tmp/;ls", (error, stdout, stderr) => {
+          console.log(`${stdout}`);
+          console.log(`${stderr}`);
+          callback(null, "OK, responsive");
+          if (error !== null) {
+            console.log(`exec error: ${error}`);
+            callback("ERROR!");
+          }
+        });
+        
+        callback(null, "Hello World");
+      }
+    );
+  });
+};
