e824438599ae6f4a74a8b5f35c22b08fba8a6669
bearcott <bear@bearcott.com>
2017-10-21 19:21:52 -0500

    testing comparisons

--- OpenCV_Comparator/main.py
+++ OpenCV_Comparator/main.py
@@ -6,7 +6,7 @@ img1 = cv2.imread('tmobile.png',0)          # queryImage
 img2 = cv2.imread('tmobile_big.png',0) # trainImage
 
 # Initiate SIFT detector
-sift = cv2.SIFT()
+sift = cv2.xfeatures2d.SIFT_create()
 
 # find the keypoints and descriptors with SIFT
 kp1, des1 = sift.detectAndCompute(img1,None)
