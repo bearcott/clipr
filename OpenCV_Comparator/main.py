80093a14a964336849971272968f23ff946eb106
bearcott <bear@bearcott.com>
2017-10-22 00:59:18 -0500

    wip opencv

--- OpenCV_Comparator/main.py
+++ OpenCV_Comparator/main.py
@@ -2,21 +2,22 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-template = cv2.imread('fjords.jpg',0)
-base = cv2.imread('fjords_big.JPG',0)
+def compareImages(base, template):
+    template = cv2.imread('fjords.jpg',0)
+    base = cv2.imread('fjords_big.JPG',0)
 
-orb = cv2.ORB_create()
+    orb = cv2.ORB_create()
 
-kp1, des1 = orb.detectAndCompute(template,None)
-kp2, des2 = orb.detectAndCompute(base,None)
+    kp1, des1 = orb.detectAndCompute(template,None)
+    kp2, des2 = orb.detectAndCompute(base,None)
 
-bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-matches = bf.match(des1,des2)
-matches = sorted(matches, key = lambda x:x.distance)
+    matches = bf.match(des1,des2)
+    matches = sorted(matches, key = lambda x:x.distance)
 
-print reduce(lambda x,y: x+y, map(lambda x: x.distance, matches[:20])) / 20
+    print reduce(lambda x,y: x+y, map(lambda x: x.distance, matches[:20])) / 20
 
-img3 = cv2.drawMatches(template,kp1,base,kp2,matches[:10],None, flags=2)
-plt.imshow(img3)
-plt.show()
+    # img3 = cv2.drawMatches(template,kp1,base,kp2,matches[:10],None, flags=2)
+    # plt.imshow(img3)
+    # plt.show()
