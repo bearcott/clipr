52ab9c8f30c8aaf504ace62802839af675e1b134
Raviteja Lingineni <rlingineni@live.com>
2017-10-21 19:21:32 -0500

    adding images

--- OpenCV_Comparator/main.py
+++ OpenCV_Comparator/main.py
@@ -1,41 +1,16 @@
-import numpy as np
 import cv2
+import numpy as np
 from matplotlib import pyplot as plt
 
-img1 = cv2.imread('tmobile.png',0)          # queryImage
-img2 = cv2.imread('tmobile_big.png',0) # trainImage
+img_rgb = cv2.imread('tmobile_big.png')
+img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+template = cv2.imread('tmobile.png',0)
+w, h = template.shape[::-1]
 
-# Initiate SIFT detector
-sift = cv2.SIFT()
+res = cv2.matchTemplate(img_gray,template,cv2.TM_SQDIFF)
+threshold = 0.8
+loc = np.where( res >= threshold)
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
 
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
-
-# FLANN parameters
-FLANN_INDEX_KDTREE = 0
-index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-search_params = dict(checks=50)   # or pass empty dictionary
-
-flann = cv2.FlannBasedMatcher(index_params,search_params)
-
-matches = flann.knnMatch(des1,des2,k=2)
-
-# Need to draw only good matches, so create a mask
-matchesMask = [[0,0] for i in xrange(len(matches))]
-
-# ratio test as per Lowe's paper
-for i,(m,n) in enumerate(matches):
-    if m.distance < 0.7*n.distance:
-        matchesMask[i]=[1,0]
-
-draw_params = dict(matchColor = (0,255,0),
-                   singlePointColor = (255,0,0),
-                   matchesMask = matchesMask,
-                   flags = 0)
-
-img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
-
-
-
-plt.imshow(img3,),plt.show()
+cv2.imwrite('res.png',img_rgb)
